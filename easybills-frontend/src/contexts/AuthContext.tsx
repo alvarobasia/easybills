51458f2835e92b77a48d54921244fa1ae39702d9
Álvaro Basílio de Araújo <alvaro.araujo@aluno.ufop.edu.br>
2021-11-21 18:23:42 -0300

    fix auth context

--- easybills-frontend/src/contexts/AuthContext.tsx
+++ easybills-frontend/src/contexts/AuthContext.tsx
@@ -1,10 +1,9 @@
 import { createContext, PropsWithChildren, useState } from "react";
-import Router from "next/router";
-
+import { loginService } from "../services/login";
+import { useToast } from "@chakra-ui/react";
+import { parseCookies, setCookie, destroyCookie } from "nookies";
 type AuthContextType = {
   isAuthenticated: boolean;
-  error: boolean;
-  loading: boolean;
   user: User | null;
   singIn: (email: string, password: string) => void;
 };
@@ -18,17 +17,28 @@ export type User = {
 export const AuthContext = createContext({} as AuthContextType);
 
 export function AuthProvider(props: PropsWithChildren<any>) {
-  const isAuthenticated = false;
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [user, setUser] = useState<User | null>(null);
-  const [loading, setLoading] = useState(false);
-  const [errorState, setErrorState] = useState(false);
+  const toast = useToast();
 
-  async function singIn(email: string, password: string) {}
+  async function singIn(email: string, password: string) {
+    try {
+      const { data } = await loginService(email, password);
+      setCookie(null, "token", data.access_token);
+      setUser({ id: data.id, username: data.name, email: data.email });
+      setIsAuthenticated(true);
+    } catch (err) {
+      toast({
+        title: "Login inválido",
+        status: "error",
+        isClosable: true,
+        duration: 5000,
+      });
+    }
+  }
 
   return (
-    <AuthContext.Provider
-      value={{ isAuthenticated, error: errorState, loading, user, singIn }}
-    >
+    <AuthContext.Provider value={{ isAuthenticated, user, singIn }}>
       {props.children}
     </AuthContext.Provider>
   );
